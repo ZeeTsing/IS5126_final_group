322ffff1b2a62fe51a7353fcb2c070ff3ef7c2a2
Xinyan Zhao <80444337+muchu@users.noreply.github.com>
2024-11-01 21:11:11 +0800

    pages templet and requirements files

---
+++ homePage.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Group 7 Final Project",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+st.header("Data-Driven Fraud Detection in Credit Card Payments")
+st.write("\n\n\n\n")
+st.header("Abstract")
+st.write("Fraud detection is a critical issue in the financial industry. In this project, we aim to build a data-driven fraud detection system for credit card payments. We analyze the transaction data and build machine learning models to predict fraudulent transactions. We also provide insights into the characteristics of fraudulent transactions and the behavior of fraudsters. The project is based on a dataset containing xx credit card transactions. ")
+st.write("\n\n\n")
